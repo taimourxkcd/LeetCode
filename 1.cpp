522a13daa8aec0def18a87dd4db4ae7009b89baa
taimourz <taimour.rkt@gmail.com>
2023-03-11 19:04:52 +0500

    init

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include<string>
+
+using namespace std;    
+
+int main() {
+    string s;
+
+    cin >> s;
+    
+    cout << "ddasfas fsfads HELLO THIS IS THE OUTPUT I AM GETTING" << s << endl;
+}
