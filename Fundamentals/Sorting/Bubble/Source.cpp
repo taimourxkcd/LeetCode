40b4c7340b3362cc6e74a4eb9f2ccd9e77be71ce
taimourz <taimour.rkt@gmail.com>
2023-04-27 21:26:11 +0500

    bubble sort

--- Fundamentals/Sorting/Bubble/Source.cpp
+++ Fundamentals/Sorting/Bubble/Source.cpp
@@ -3,51 +3,44 @@
 #include <vector>
 #include <unordered_map>
 #include <string>
-#include<queue>
+#include <queue>
 using namespace std;
-
-
 
 int main()
 {
 
-    FILE* inputFile = nullptr;
-    FILE* outputFile = nullptr;
+    FILE *inputFile = nullptr;
+    FILE *outputFile = nullptr;
     freopen_s(&inputFile, "input.txt", "r", stdin);    // Redirect standard input to input.txt
     freopen_s(&outputFile, "output.txt", "w", stdout); // Redirect standard output to output.txt
 
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
 
         int n;
         cin >> n;
         vector<int> nums(n);
-        
-        for (int i = 0; i < n; i++) {
+
+        for (int i = 0; i < n; i++)
+        {
             cin >> nums[i];
         }
 
-    
-        for (int i = 0; i < n; i++) {
-            nums[i] = nums[i] * nums[i];
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[j] < nums[i]) {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[j] < nums[i])
+                {
                     swap(nums[i], nums[j]);
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             cout << nums[i] << " ";
         }
-
-
-
-
-
-
-
     }
 }
